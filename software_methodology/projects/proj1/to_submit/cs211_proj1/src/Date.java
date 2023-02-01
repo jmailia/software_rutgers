ae1fb0ff4e7b227fb521e8570076de8ab13f9449
Aidan Cronin <104910154+AidanJCronin@users.noreply.github.com>
2023-02-01 14:48:40 -0500

    Update Date.java

--- software_methodology/projects/proj1/to_submit/cs211_proj1/src/Date.java
+++ software_methodology/projects/proj1/to_submit/cs211_proj1/src/Date.java
@@ -1,23 +1,90 @@
-public class Date implements Comparable<Date> {
+import java.util.Calendar;
+
+public class Date {
     private int year;
     private int month;
     private int day;
-    public Date() { //create an object with today’s date (see Calendar class)
-        this.year = 2000;
-        this.month = 9;
-        this.day = 5;
+
+    public static final int QUADRENNIAL = 4;
+    public static final int CENTENNIAL = 100;
+    public static final int QUATERCENTENNIAL = 400;
+
+    public int getYear(){
+        return this.year;
     }
-    public Date(String date) { //take “mm/dd/yyyy” and create a Date object
-        this.year = 2000;
-        this.month = 9;
-        this.day = 5;
+    public int getMonth(){
+        return this.month;
+    }
+    public int getDay(){
+        return this.day;
     }
 
-    public String printDate() {
-        return this.day + "/" + this.month + "/" + this.year;
+    public Date() {
+        Calendar calendar = Calendar.getInstance();
+        int month = calendar.get(Calendar.MONTH);
+        month++;
+        this.month = month;
+        this.day = calendar.get(Calendar.DATE);
+        this.year = calendar.get(Calendar.YEAR);
     }
-    public boolean isValid() {return true;} //check if a date is a valid calendar date
-    @Override public int compareTo(Date ree) {
-        return this.month - ree.month;
+
+    public Date(int month, int day, int year){
+        this.month = month;
+        this.day = day;
+        this.year = year;
+    }
+
+    public Date(String date) {
+        String[] arrDate = date.split("/");
+        this.month = Integer.parseInt(arrDate[0]);
+        this.day = Integer.parseInt(arrDate[1]);;
+        this.year = Integer.parseInt(arrDate[2]);;
+    }
+
+    public boolean isValid() {
+        if (this.month > 12){
+            return false;
+        }
+        switch(this.month){
+            case 1, 3, 5, 7, 8, 10, 12:
+                if(this.day > 31){
+                    return false;
+                }
+                break;
+            case 4, 6, 9, 11:
+                if (this.day > 30){
+                    return false;
+                }
+                break;
+            case 2:
+                if (this.year % QUADRENNIAL == 0){
+                    if (this.year % CENTENNIAL == 0){
+                        if (this.year % QUATERCENTENNIAL == 0){
+                            if (this.day > 29){
+                                return false;
+                            }
+                        }
+                    }
+                    else
+                        if (this.day > 29){
+                            return false;
+                        }
+                }
+                else
+                    if (this.day > 28){
+                        return false;
+                    }
+                break;
+        }
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.YEAR, -16);
+        if (calendar.get(Calendar.YEAR) > this.year){
+            return true;
+        }
+        if (calendar.get(Calendar.YEAR) == this.year){
+            if (calendar.get(Calendar.MONTH) > this.month){
+                
+            }
+        }
     }
 }
