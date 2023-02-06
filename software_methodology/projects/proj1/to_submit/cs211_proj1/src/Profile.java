eec939794af44298a23732a46a71a7e7870cbe63
Aidan Cronin <104910154+AidanJCronin@users.noreply.github.com>
2023-02-05 19:26:09 -0500

    Update Profile.java

--- software_methodology/projects/proj1/to_submit/cs211_proj1/src/Profile.java
+++ software_methodology/projects/proj1/to_submit/cs211_proj1/src/Profile.java
@@ -44,7 +44,7 @@ public class Profile implements Comparable<Profile> {
      * Method returns the string version of profile.
      * @return String
      */
-    public String printProfile() {
+    public String toString() {
         return fname + " " + lname + " " + dob.toString();
     }
 
@@ -73,9 +73,9 @@ public class Profile implements Comparable<Profile> {
     }
 
     /**
-     * Method to compare two students together. Returns 1 if original student
-     * is alphabetically before compared student, returns 2 if original student
-     * is alphabetically after compared student, returns 0 if equal.
+     * Method to compare two profiles together. Returns 1 if original profile
+     * is alphabetically before compared profile, returns 2 if original profile
+     * is alphabetically after compared profile, returns 0 if equal.
      * @param profile
      * @return int
      */
@@ -88,22 +88,22 @@ public class Profile implements Comparable<Profile> {
         else
             min = stu1.length();
         for(int i = 0; i < min; i++){
-            if(Character.toUpperCase(stu1.charAt(i)) < Character.toUpperCase(stu2.charAt(i))){
+            if(Character.toUpperCase(stu1.charAt(i)) < Character.toUpperCase(stu2.charAt(i))){ //stu1 is alphabetically before stu2
                 return 1;
             }
-            if(Character.toUpperCase(stu1.charAt(i)) == Character.toUpperCase(stu2.charAt(i))){
+            if(Character.toUpperCase(stu1.charAt(i)) == Character.toUpperCase(stu2.charAt(i))){ //Characters at this index are equal
                 continue;
             }
-            if(Character.toUpperCase(stu1.charAt(i)) > Character.toUpperCase(stu2.charAt(i))){
+            if(Character.toUpperCase(stu1.charAt(i)) > Character.toUpperCase(stu2.charAt(i))){ //stu1 is alphabetically after stu2
                 return -1;
             }
         }
-        if(this.dob.compareTo(profile.getDob()) == 1){
+        if(this.dob.compareTo(profile.getDob()) == 1){ //stu1 dob is after stu2
             return -1;
         }
-        if(this.dob.compareTo(profile.getDob()) == -1){
+        if(this.dob.compareTo(profile.getDob()) == -1){//stu1 dob is before stu2
             return 1;
         }
-        return 0;
+        return 0; //profiles are equal
     }
 }
