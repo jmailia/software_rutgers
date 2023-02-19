276b6885169c035080846b0627d78ce0d082f4c9
Aidan Cronin <104910154+AidanJCronin@users.noreply.github.com>
2023-02-18 19:34:08 -0500

    Update Resident.java

--- software_methodology/projects/CS213Project2/src/Resident.java
+++ software_methodology/projects/CS213Project2/src/Resident.java
@@ -1,7 +1,38 @@
 public class Resident extends Student {
+    
     private int scholarship;
+
     public Resident(Profile profile, Major major, int creditCompleted, int scholarship){
         super(profile, major, creditCompleted);
         this.scholarship = scholarship;
     }
-}
+    
+        public double tuitionDue(int creditsEnrolled){
+            if(isValid(creditsEnrolled)){
+                double tuition = 0;
+                int universityFee = 3268;
+                int creditsPerHour = 404;
+                int exceededCredits = 16;
+                int fulltimeCredits = 12;
+                int fulltimeTuition = 12536;
+                if(creditsEnrolled < fulltimeCredits){ //Parttime students
+                    double parttimeFeePercentage = 0.8;
+                    tuition = (parttimeFeePercentage * universityFee) + (creditsPerHour * creditsEnrolled);
+                    return tuition;
+                }
+                if(creditsEnrolled > exceededCredits){ //Students with over 16 credits
+                    int extraCredits = creditsEnrolled - exceededCredits;
+                    tuition = (extraCredits * creditsPerHour) + universityFee + fulltimeTuition;
+                    return tuition;
+                } // Fulltime students who don't exceed 16 credits
+                tuition = universityFee + fulltimeTuition;
+                return tuition;
+            }
+            return -1; //credits not valid
+        }
+
+        public boolean isResident(){
+            return true;
+        }
+    }
+    
