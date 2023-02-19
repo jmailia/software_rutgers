7abed3f00cc550010a4dc615addd70ebea1bec43
Aidan Cronin <104910154+AidanJCronin@users.noreply.github.com>
2023-02-18 20:01:13 -0500

    Update Resident.java

--- software_methodology/projects/CS213Project2/src/Resident.java
+++ software_methodology/projects/CS213Project2/src/Resident.java
@@ -1,45 +1,36 @@
-public class Resident extends Student {
+/* @author Henry Hecht */
+/* @author Aidan Cronin */
 
-    private int scholarship;
-
-    public Resident(Profile profile, Major major, int creditCompleted){
+public class NonResident extends Student {
+    public NonResident(Profile profile, Major major, int creditCompleted){
         super(profile, major, creditCompleted);
-        this.scholarship = 0;
-    }
-
-    public void setScholarship(int scholarship) {
-        this.scholarship = scholarship;
     }
 
     public double tuitionDue(int creditsEnrolled){
         if(isValid(creditsEnrolled)){
             double tuition = 0;
             int universityFee = 3268;
-            int creditsPerHour = 404;
+            int creditsPerHour = 966;
             int exceededCredits = 16;
             int fulltimeCredits = 12;
-            int fulltimeTuition = 12536;
+            int fulltimeTuition = 29737;
             if(creditsEnrolled < fulltimeCredits){ //Parttime students
                 double parttimeFeePercentage = 0.8;
                 tuition = (parttimeFeePercentage * universityFee) + (creditsPerHour * creditsEnrolled);
-                tuition -= this.scholarship;
                 return tuition;
             }
             if(creditsEnrolled > exceededCredits){ //Students with over 16 credits
                 int extraCredits = creditsEnrolled - exceededCredits;
                 tuition = (extraCredits * creditsPerHour) + universityFee + fulltimeTuition;
-                tuition -= this.scholarship;
                 return tuition;
             } // Fulltime students who don't exceed 16 credits
             tuition = universityFee + fulltimeTuition;
-            tuition -= this.scholarship;
             return tuition;
         }
         return -1; //credits not valid
     }
 
     public boolean isResident(){
-            return true;
-        }
+        return false;
+    }
 }
-
