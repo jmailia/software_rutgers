947ce1be9f222d998d500f4f4bb437e458d13065
Aidan Cronin <104910154+AidanJCronin@users.noreply.github.com>
2023-02-20 14:42:33 -0500

    Update RunProject2.java

--- software_methodology/projects/CS213Project2/src/RunProject2.java
+++ software_methodology/projects/CS213Project2/src/RunProject2.java
@@ -1,8 +1,10 @@
 /* @author Henry Hecht */
 /* @author Aidan Cronin */
 
+import java.io.FileNotFoundException;
+
 public class RunProject2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
         new TuitionManager().run();
     }
