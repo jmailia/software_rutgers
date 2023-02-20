92918f1fcba72c68a90e5980e430a39fe811b252
Aidan Cronin <104910154+AidanJCronin@users.noreply.github.com>
2023-02-20 13:43:51 -0500

    Update TuitionManager.java

--- software_methodology/projects/CS213Project2/src/TuitionManager.java
+++ software_methodology/projects/CS213Project2/src/TuitionManager.java
@@ -1,39 +1,64 @@
 /* @author Henry Hecht */
 /* @author Aidan Cronin */
 
+import java.io.File;
 import java.util.Scanner;
 
 public class TuitionManager {
 
-    private void LS_Command(String file) {
+    private void LS_Command(Scanner file) {
+        Enrollment temp = new Enrollment();
+        while(file.hasNextLine()) {
+            String line = file.nextLine();
+            String[] lineInputs = line.split(",");
+            switch (lineInputs[0]) {
+                case "R":
+                    Date tempDateR = new Date(lineInputs[3]);
+                    Profile tempProfileR = new Profile(lineInputs[2], lineInputs[1], tempDateR);
+                    Resident resident = new Resident(tempProfileR, lineInputs[4], Integer.parseInt(lineInputs[5]));
+                    break;
+                case "I":
+                    Date tempDateI = new Date(lineInputs[3]);
+                    Profile tempProfileI = new Profile(lineInputs[2], lineInputs[1], tempDateI);
+                    break;
+                case "T":
+                    Date tempDateT = new Date(lineInputs[3]);
+                    Profile tempProfileT = new Profile(lineInputs[2], lineInputs[1], tempDateT);
+                    break;
+                case "N":
+                    Date tempDateN = new Date(lineInputs[3]);
+                    Profile tempProfileN = new Profile(lineInputs[2], lineInputs[1], tempDateN);
+                    break;
+            }
+        }
 
     }
 
-    private void AR_Command(String school, Roster myRoster) {
+    private void AR_Command() {
 
     }
 
-    private void AN_Command(String school, Roster myRoster) {
+    private void AN_Command() {
 
     }
 
-    private void AT_Command(String school, Roster myRoster) {
+    private void AT_Command() {
 
     }
 
-    private void AI_Command(String fname, String lname, Date date, Major major, int creditsCompleted) {
+    private void AI_Command() {
 
     }
 
-    private void E_Command(String school, Roster myRoster) {
+    private void E_Command() {
 
     }
 
-    private void D_Command(String school, Roster myRoster) {
+    private void D_Command() {
 
     }
 
-    private void S_Command(String school, Roster myRoster) {
+    private void S_Command() {
 
     }
 
@@ -51,52 +76,53 @@ public class TuitionManager {
 
     public void run(){
         boolean[] firstTimeLastTime = {true,false};
-        Roster myRoster = new Roster();
+        Scanner file = new Scanner(new File("studentList.txt"));
+        Enrollment enrollStudents = new Enrollment();
         Scanner sc = new Scanner(System.in);
         while(! (firstTimeLastTime[1] && sc.hasNextLine())) {                           //continuously read the line commands until the user quits
             String[] parsedCommandArguments = sc.nextLine().split("\\s+"); //parses arguments
             if (firstTimeLastTime[0]) {
-                System.out.println("Roster Manager running...\n");    //user knows software is ready for commands
+                System.out.println("Tuition Manager running...\n");    //user knows software is ready for commands
                 firstTimeLastTime[0] = false;
             }
-            switch (parsedCommandArguments[0]) {     //TODO: tweak                            //get the current command\
+            switch (parsedCommandArguments[0]) {                                //get the current command\
                 case "":        //if the enter key is pressed without any input, this prevents an error
                     break;
                 case "LS":
-                    LS_Command(parsedCommandArguments[1],parsedCommandArguments[2],parsedCommandArguments[3],parsedCommandArguments[4],parsedCommandArguments[5],myRoster);
+                    LS_Command(file);
                     break;
                 case "AR":
-                    AR_Command(parsedCommandArguments[1],parsedCommandArguments[2],parsedCommandArguments[3],myRoster);
+                    AR_Command();
                     break;
                 case "AN":
-                    AN_Command(parsedCommandArguments[1],parsedCommandArguments[2],parsedCommandArguments[3],myRoster);
+                    AN_Command();
                     break;
                 case "AT":
-                    AT_Command(parsedCommandArguments[1],parsedCommandArguments[2],parsedCommandArguments[3],myRoster);
+                    AT_Command();
                     break;
                 case "AI":
-                    AT_Command(parsedCommandArguments[1],parsedCommandArguments[2],parsedCommandArguments[3],myRoster);
+                    AT_Command();
                     break;
                 case "E":
-                    E_Command(parsedCommandArguments[0],myRoster);
+                    E_Command();
                     break;
                 case "D":
-                    D_Command(parsedCommandArguments[1],myRoster);
+                    D_Command();
                     break;
                 case "S":
-                    S_Command(parsedCommandArguments[1],parsedCommandArguments[2],parsedCommandArguments[3],parsedCommandArguments[4],myRoster);
+                    S_Command();
                     break;
                 case "PE":
-                    PE_Command(parsedCommandArguments[1],parsedCommandArguments[2],parsedCommandArguments[3],parsedCommandArguments[4],myRoster);
+                    PE_Command();
                     break;
                 case "PT":
-                    PT_Command(parsedCommandArguments[1],parsedCommandArguments[2],parsedCommandArguments[3],parsedCommandArguments[4],myRoster);
+                    PT_Command();
                     break;
                 case "SE":
-                    SE_Command(parsedCommandArguments[1],parsedCommandArguments[2],parsedCommandArguments[3],parsedCommandArguments[4],myRoster);
+                    SE_Command();
                     break;    
                 case "Q":
-                    System.out.println("Roster Manager terminated.");    //user has terminated the software normally
+                    System.out.println("Tuition Manager terminated.");    //user has terminated the software normally
                     firstTimeLastTime[1] = true;
                     break;
                 default:
