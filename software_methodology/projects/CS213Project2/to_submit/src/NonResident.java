22b2f742116ea88ff830b96e2615c76a9fc43fbb
Aidan Cronin <104910154+AidanJCronin@users.noreply.github.com>
2023-02-22 18:50:53 -0500

    Update NonResident.java

--- software_methodology/projects/CS213Project2/to_submit/src/NonResident.java
+++ software_methodology/projects/CS213Project2/to_submit/src/NonResident.java
@@ -6,31 +6,35 @@ public class NonResident extends Student {
         super(profile, major, creditCompleted);
     }
 
-    /**
-     * The tuition due for nonresident students
-     * @param creditsEnrolled The number of credits the nonresident student is enrolled in
-     * @return the tuition which the student is to pay
-     */
     public double tuitionDue(int creditsEnrolled){
         if(isValid(creditsEnrolled)){
+            double tuition = 0;
             int universityFee = 3268;
             int creditsPerHour = 966;
             int exceededCredits = 16;
             int fulltimeCredits = 12;
             int fulltimeTuition = 29737;
             if(creditsEnrolled < fulltimeCredits){ //Parttime students
-                return (0.8 * universityFee) + (creditsPerHour * creditsEnrolled);
+                double parttimeFeePercentage = 0.8;
+                tuition = (parttimeFeePercentage * universityFee) + (creditsPerHour * creditsEnrolled);
+                return tuition;
             }
             if(creditsEnrolled > exceededCredits){ //Students with over 16 credits
                 int extraCredits = creditsEnrolled - exceededCredits;
-                return (extraCredits * creditsPerHour) + universityFee + fulltimeTuition;
+                tuition = (extraCredits * creditsPerHour) + universityFee + fulltimeTuition;
+                return tuition;
             } // Fulltime students who don't exceed 16 credits
-            return universityFee + fulltimeTuition;
+            tuition = universityFee + fulltimeTuition;
+            return tuition;
         }
         return -1; //credits not valid
     }
 
     public boolean isResident(){
         return false;
+    }
+
+    public void setScholarship(int scholarship){
+        return;
     }
 }
