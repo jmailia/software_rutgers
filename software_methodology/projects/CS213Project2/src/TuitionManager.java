5f8892ea1f22c6aa4cddee7449e1c804c5dce355
Aidan Cronin <104910154+AidanJCronin@users.noreply.github.com>
2023-02-22 15:49:46 -0500

    Update TuitionManager.java

--- software_methodology/projects/CS213Project2/src/TuitionManager.java
+++ software_methodology/projects/CS213Project2/src/TuitionManager.java
@@ -247,30 +247,149 @@ public class TuitionManager {
         }
     }
 
-    private void E_Command(String fname, String lname, String date, int creditsEnrolled, Enrollment myEnrollment, Roster myRoster) { //TODO: Just started
-        Profile profile = new Profile(lname, fname, new Date(date));
+    /**
+     * Removes a student from Roster, but only if they are already in the Roster
+     * @param firstName First name of the student who might be removed
+     * @param lastName Last name of the student who might be removed
+     * @param DOB DOB of the student who might be removed
+     * @param myRoster Roster which might contain the student who, if in it, is to be removed
+     */
+    private void R_Command(String firstName, String lastName, String DOB, Roster myRoster) {
+        Profile profileToRemove = new Profile(lastName, firstName, new Date(DOB));
         if (myRoster!=null) {
             for (Student student : myRoster.getRoster()) {
                 if (student != null) {
+                    if (student.getProfile().equals(profileToRemove)) {
+                        myRoster.remove(student);
+                        System.out.println(profileToRemove.toString() + " removed from the roster.");
+                        return;
+                    }
+                }
+            }
+            System.out.println(profileToRemove.toString() + " is not in the roster.");
+        }
+        System.out.println("Roster is empty.");
+    }
+
+    private void E_Command(String fname, String lname, String date, int creditsEnrolled, Enrollment myEnrollment, Roster myRoster) {
+        Profile profile = new Profile(lname, fname, new Date(date));
+        if (myRoster!=null) {
+            for (Student student : myRoster.getRoster()) { //Check to see if student is in roster
+                if (student != null) {
                     if (student.getProfile().equals(profile)) {
+                        for (EnrollStudent enrollStudent : myEnrollment.getEnrollStudents()) { //Check to see if student is already enrolled
+                            if (student != null) {
+                                if (student.getProfile().equals(profile)) {
+                                    enrollStudent.setCreditsEnrolled(creditsEnrolled);
+                                    System.out.println(profile.toString() + " enrolled " + creditsEnrolled + " credits");
+                                    return;
+                                }
+                            }
+                        }
                         EnrollStudent enrollStudent = new EnrollStudent(profile, creditsEnrolled);
                         myEnrollment.add(enrollStudent);
                         System.out.println(profile.toString() + " enrolled " + creditsEnrolled + " credits");
+                        return;
                     }
                 }
             }
             System.out.println("Cannot enroll: " + profile.toString() + " is not in the roster.");
         }
-        System.out.println("Roster is empty!");
-
+        System.out.println("Enrollment is empty!");
     }
 
-    private void D_Command(String fname, String lname, String date) {
-
+    private void D_Command(String fname, String lname, String date, Enrollment myEnrollment) {
+        Profile profile = new Profile(lname, fname, new Date(date));
+        if (myEnrollment!=null) {
+            for (EnrollStudent student : myEnrollment.getEnrollStudents()) {
+                if (student != null) {
+                    if (student.getProfile().equals(profile)) {
+                        myEnrollment.remove(student);
+                        System.out.println(profile.toString() + " dropped.");
+                        return;
+                    }
+                }
+            }
+            System.out.println(profile.toString() + " is not enrolled.");
+        }
+        System.out.println("Enrollment is empty!");
     }
 
     private void S_Command(String fname, String lname, String date, int scholarship, Enrollment myEnrollment, Roster myRoster) {
 
+    }
+
+    /**
+     * List the students in a specified school, sorted by last name, first name, and DOB
+     * @param school the specified school, case-insensitive, of which we want to list its enrollment
+     * @param myRoster the roster containing all current students
+     */
+    private void L_Command(String school, Roster myRoster) {
+
+        boolean validSchool = false;
+        Roster tempRoster = new Roster();
+
+        Major majorToList = Major.MATH;
+        if (school.toUpperCase().equals("SOE")) {
+            majorToList = Major.EE;
+            validSchool = true;
+        } else if (school.toUpperCase().equals("SC&I")) {
+            majorToList = Major.ITI;
+            validSchool = true;
+        } else if (school.toUpperCase().equals("RBS")) {
+            majorToList = Major.BAIT;
+            validSchool = true;
+        } else if (school.toUpperCase().equals("SAS")) {
+            validSchool = true;
+        }
+        if (validSchool) {
+            for (int k = 0; k < myRoster.getRoster().length; k++) {
+                if (myRoster.getRoster()[k] != null) {
+                    // SAS has both MATH and CS, so this if statement for CS is needed
+                    // The MATH option runs in the second if statement as majorToList is set to this when initialized
+                    if ((school.toUpperCase().equals("SAS")) && (myRoster.getRoster()[k].getMajor() == Major.CS)) {
+                        tempRoster.add(myRoster.getRoster()[k]);
+                    } else if (myRoster.getRoster()[k].getMajor() == majorToList) {
+                        tempRoster.add(myRoster.getRoster()[k]);
+                    }
+                }
+
+            }
+            System.out.println("* Students in " + school + " *");
+            tempRoster.print();
+            System.out.println("* end of list **");
+        } else {
+            System.out.println("School doesn't exist: " + school);
+        }
+    }
+
+    /**
+     * Change the students major, first testing whether the major is valid and the student is in the roster.
+     * @param firstName First name of the student who wants to change their major
+     * @param lastName Last name of the student who wants to change their major
+     * @param DOB Date of birth of the student who wants to change their major
+     * @param majorChangingTo The major which the student wants to change to
+     * @param myRoster The roster which might contain the student who wants to change their major
+     */
+    private void C_Command(String firstName, String lastName, String DOB, String majorChangingTo, Roster myRoster) {
+
+        boolean isMajorInvalid = true;
+        for (Major validMajor : Major.values()) {
+            if (validMajor.name().equals(majorChangingTo.toUpperCase())) {
+                isMajorInvalid = false;
+                Profile profileOfStudent = new Profile(lastName, firstName, new Date(DOB));
+                if (myRoster.contains(new Student (profileOfStudent,validMajor,1))) {
+                    System.out.println(profileOfStudent.toString() + " major changed to " + majorChangingTo);
+                } else {
+                    System.out.println(profileOfStudent.toString() + " is not in the roster.");
+                }
+                break;
+            }
+        }
+
+        if (isMajorInvalid) {
+            System.out.println("Major code invalid: " + majorChangingTo);
+        }
     }
 
     private void PE_Command(Enrollment myEnrollment) {
@@ -285,12 +404,31 @@ public class TuitionManager {
 
     }
 
-    private void P_Command(Roster myRoster) {
+    /**
+     * Display the roster in various sortings; this is the method for P, PS, and PC
+     * @param whichP a String of either "P", "PS", or "PC"
+     * @param myRoster the Roster we want to print
+     */
+    private void P_Command(String whichP, Roster myRoster) {
+
         if (myRoster == null || myRoster.getRoster() == null) {
             System.out.println("Student roster is empty!");
-            return;
         }
-        myRoster.print();
+        else if (myRoster.getRoster()[0] == null) { //All students in an already established Student[] have been removed
+            System.out.println("Student roster is empty!");
+        } else {
+            if (whichP.equals("P")) {
+                System.out.println("* Student roster sorted by last name, first name, DOB **");
+                myRoster.print();
+            } else if (whichP.equals("PS")) {
+                System.out.println("* Student roster sorted by standing **");
+                myRoster.printByStanding();
+            } else {
+                System.out.println("* Student roster sorted by school, major **");
+                myRoster.printBySchoolMajor();
+            }
+            System.out.println("* end of roster **");
+        }
     }
 
     private void SE_Command() {
@@ -327,23 +465,34 @@ public class TuitionManager {
                 case "AI":
                     AI_Command(input[1], input[2], input[3], input[4],Integer.parseInt(input[5]), (input[6] == "true"), myRoster);
                     break;
+                case "R":
+                    R_Command(input[1],input[2],input[3],myRoster);
+                    break;
                 case "E":
                     E_Command(input[1], input[2], input[3], Integer.parseInt(input[5]), myEnrollment, myRoster);
                     break;
                 case "D":
-                    D_Command(input[1], input[2], input[3]);
+                    D_Command(input[1], input[2], input[3], myEnrollment);
                     break;
                 case "S":
                     S_Command(input[1], input[2], input[3], Integer.parseInt(input[4]), myEnrollment, myRoster);
+                    break;
+                case "P":
+                case "PS":
+                case "PC":
+                    P_Command(input[0],myRoster);
+                    break;
+                case "L":
+                    L_Command(input[1],myRoster);
+                    break;
+                case "C":
+                    C_Command(input[1],input[2],input[3],input[4],myRoster);
                     break;
                 case "PE":
                     PE_Command(myEnrollment);
                     break;
                 case "PT":
                     PT_Command();
-                    break;
-                case "P":
-                    P_Command(myRoster);
                     break;
                 case "SE":
                     SE_Command();
