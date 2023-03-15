147a0135e49ed9633ba3f4bb4167b5dff831bcda
Aidan Cronin <104910154+AidanJCronin@users.noreply.github.com>
2023-03-15 16:10:05 -0400

    Delete software_methodology/projects/proj3/to_submit directory

--- software_methodology/projects/proj3/to_submit/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.cs213project3 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.cs213project3 to javafx.fxml;
-    exports com.example.cs213project3;
-}
